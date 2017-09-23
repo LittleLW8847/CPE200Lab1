b912849a3275f4a48b979f521259321b8bdb35c0
Ohm <LittleLW000@gmail.com>
2017-09-22 17:41:03 -0700

    Add project files.

---
+++ CPE200Lab1/RPNCalculatorEngine.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPE200Lab1
+{
+    class RPNCalculatorEngine : CalculatorEngine
+    {
+        public new string Process(string str)
+        {
+            Stack<string> rpnStack = new Stack<string>();
+            List<string> parts = str.Split(' ').ToList<string>();
+            string result;
+            string firstOperand, secondOperand;
+
+            foreach (string token in parts)
+            {
+                if (isNumber(token))
+                {
+                    rpnStack.Push(token);
+                }
+                else if (isOperator(token))
+                {
+                    //FIXME, what if there is only one left in stack?
+                    secondOperand = rpnStack.Pop();
+                    firstOperand = rpnStack.Pop();
+                    result = calculate(token, firstOperand, secondOperand);
+                    rpnStack.Push(result);
+                    if (result == "E")
+                    {
+                        return result;
+                    }
+                    rpnStack.Push(result);
+                }else if(token == "1/x" || token == "sqrt")
+                {
+                    firstOperand = rpnStack.Pop();
+                    result = unaryCalculate(token, firstOperand);
+                    rpnStack.Push(result);
+                }
+                else if(token == "%")
+                {
+                    secondOperand = rpnStack.Pop();
+
+                    if(rpnStack.Count == 0)
+                    {
+                        return "E";
+                    }
+                    firstOperand = rpnStack.Pop();
+                    result = calculate(token, firstOperand, secondOperand);
+                    rpnStack.Push(result);
+                }
+            }
+            //FIXME, what if there is more than one, or zero, items in the stack?
+            result = rpnStack.Pop();
+            return result;
+        }
+    }
+}
